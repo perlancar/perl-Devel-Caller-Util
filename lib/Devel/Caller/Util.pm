72123deaf9471a92fb49f2342852ecd0ce9c3efb
perlancar (@pc-home) <perlancar@gmail.com>
2015-07-26 10:54:33 +0700

    Initial commit

---
+++ lib/Devel/Caller/Util.pm
@@ -0,0 +1,38 @@
+package Devel::Caller::Util;
+
+# DATE
+# VERSION
+
+use 5.010001;
+use warnings;
+use strict;
+
+use Exporter qw(import);
+our @EXPORT_OK = qw(caller);
+
+sub callers {
+    my $start = shift // 0;
+
+    my @res;
+    my $i = $start;
+    while (my @caller = caller($i)) {
+        push @res, \@caller;
+        $i++;
+    }
+
+    @res;
+}
+
+1;
+# ABSTRACT: caller()-related utility routines
+
+=head1 SYNOPSIS
+
+ use Devel::Util::Caller qw(callers);
+
+ my @callers = callers();
+
+
+=head1 SEE ALSO
+
+=cut
