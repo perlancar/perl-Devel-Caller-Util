4c3e6df44873c05932295ad4ea922fda6b97dfac
perlancar (@pc-home) <perlancar@gmail.com>
2015-07-26 11:34:47 +0700

    Add second arg 'with_args' to retrieve sub arguments, add doc

--- lib/Devel/Caller/Util.pm
+++ lib/Devel/Caller/Util.pm
@@ -11,13 +11,26 @@ use Exporter qw(import);
 our @EXPORT_OK = qw(callers);
 
 sub callers {
-    my $start = shift // 0;
+    my $start = $_[0] // 0;
+    my $with_args = $_[1];
 
     my @res;
     my $i = $start+1;
-    while (my @caller = caller($i)) {
-        push @res, \@caller;
-        $i++;
+
+    if ($with_args) {
+        {
+            package DB;
+            while (my @caller = caller($i)) {
+                $caller[11] = [@DB::args];
+                push @res, \@caller;
+                $i++;
+            }
+        }
+    } else {
+        while (my @caller = caller($i)) {
+            push @res, \@caller;
+            $i++;
+        }
     }
 
     @res;
@@ -35,7 +48,21 @@ sub callers {
 
 =head1 FUNCTIONS
 
-=head2 callers([ $n ]) => LIST
+=head2 callers([ $start=0 [, $with_args] ]) => LIST
+
+A convenience function to return the whole callers stack, produced by calling
+C<caller()> repeatedly from frame C<$start+1> until C<caller()> returns empty.
+Result will be like:
+
+ (
+     #  0          1           2       3             4          5            6           7             8        9          10
+     [$package1, $filename1, $line1, $subroutine1, $hasargs1, $wantarray1, $evaltext1, $is_require1, $hints1, $bitmask1, $hinthash1],
+     [$package2, $filename2, $line2, ...],
+     ...
+ )
+
+If C<$with_args> is true, will also return subroutine arguments in the 11th
+element of each frame, produced by retrieving C<@DB::args>.
 
 
 =head1 SEE ALSO
